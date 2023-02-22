9610784efac32fc4455a447cf529d43db716eb22
Sagar Trivedi <trivedisagartrivedi@gmail.com>
2023-02-22 14:05:50 +0530

    Add files via upload

---
+++ DownloadScript.ps1
@@ -0,0 +1,5 @@
+﻿$packerPath = "c:\packer.zip"
+#Download Packer
+Invoke-WebRequest "https://releases.hashicorp.com/packer/1.8.6/packer_1.8.6_windows_386.zip" -OutFile $packerPath
+#Unzip
+Expand-Archive -Path $packerPath -DestinationPath "c:\tmp" -Force
