d2d614a798cf7663a6bc433d1407242b19636743
Sagar Trivedi <trivedisagartrivedi@gmail.com>
2023-05-23 13:10:57 +0530

    Update DownloadScript.ps1

--- DownloadScript.ps1
+++ DownloadScript.ps1
@@ -1,5 +1,1 @@
-﻿$packerPath = "c:\packer.zip"
-#Download Packer
-Invoke-WebRequest "https://releases.hashicorp.com/packer/1.8.6/packer_1.8.6_windows_386.zip" -OutFile $packerPath
-#Unzip
-Expand-Archive -Path $packerPath -DestinationPath "c:\tmp" -Force
+return 0
